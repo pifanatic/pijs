564e7a4760fda0ef94de112077afc44b92ffc384
Kai Mörker <kai.moerker@gmail.com>
2020-05-31 00:36:41 +0200

    add tests for set and get

---
+++ test/getSet.spec.js
@@ -0,0 +1,45 @@
+import PiJS from "../js/pi.js";
+
+describe("Get/Set", () => {
+    let el,
+        view;
+
+    beforeEach(() => {
+        el = document.createElement("div");
+        el.setAttribute("id", "foo");
+        el.innerHTML = "<foo></foo>";
+
+        PiJS.register({
+            name: "FooView",
+            tagName: "foo",
+            template: "<div> Foobar </div>"
+        });
+
+        document.body.appendChild(el);
+        PiJS.init({ el: "#foo" });
+
+        view = PiJS.mainView.subviews[0].view;
+    });
+
+    afterEach(() => {
+        PiJS._unregisterAll();
+
+        document.body.removeChild(el);
+    });
+
+    describe("Set", () => {
+        it("should set value correctly", () => {
+            view.set("foo", "bar");
+
+            expect(view._attributes["foo"]).to.equal("bar");
+        });
+    });
+
+    describe("Get", () => {
+        it("should return correct value", () => {
+            view._attributes["foo"] = "bar";
+
+            expect(view.get("foo")).to.equal("bar");
+        });
+    });
+});
